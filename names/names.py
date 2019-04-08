d27e33a87b00befda3b4dc3045da75406d805d7b
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-04-08 07:32:25 -0500

    Finished names challenge with stretch

--- names/names.py
+++ names/names.py
@@ -1,5 +1,4 @@
 import time
-
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,10 +10,18 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+names_dictionary = {}
+
+for name in names_1:
+    names_dictionary[name] = 1
+
+for name in names_2:
+    try:
+        if names_dictionary[name]: duplicates.append(name)
+    except:
+        continue
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
